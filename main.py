49b89bf3c4a6e88b33447e2de0d90034a29e2d3b
brennerhaverlock <brenner.haverlock@gmail.com>
2020-09-30 11:49:06 -0400

    init
    added get_file_lines

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+import os
+
+#Create a function called get_file_lines()
+
+def get_file_lines(filename):
+    return open(filename, 'r').read()#.strip().split('\n')
+
+print(get_file_lines('poem.txt'))
+#Create a function called lines_printed_backwards()
+
+
+
+#Create a function called lines_printed_random():
+
+#Create a function called lines_printed_custom()
