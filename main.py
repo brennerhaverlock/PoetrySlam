13ba438e2f90970942f01b2fa8ad5f16278b7084
brennerhaverlock <brenner.haverlock@gmail.com>
2020-09-30 17:13:34 -0400

    line
    implement backword lines

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ import os
 def get_file_lines(filename):
     return open(filename, 'r').read()#.strip().split('\n')
 
-print(get_file_lines('poem.txt'))
+#print(get_file_lines('poem.txt'))
 #Create a function called lines_printed_backwards()
 
+def lines_printed_backwords(lines_list):
+    textfile = open("poem.txt")
+    lines = textfile.readlines()
+    for line in reversed(lines):
+        return line
 
+print(lines_printed_backwords(get_file_lines('poem.txt')))
 
 #Create a function called lines_printed_random():
 
